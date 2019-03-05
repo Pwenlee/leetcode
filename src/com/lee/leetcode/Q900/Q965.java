c1c0799791786c6c3a754397271593b5c85fc2c8
Pwenlee <630238086@qq.com>
2019-03-05 21:38:54 +0800

    900 965

---
+++ src/com/lee/leetcode/Q900/Q965.java
@@ -0,0 +1,36 @@
+package com.lee.leetcode.Q900;
+
+import java.util.Objects;
+
+/**
+ * @author Administrator
+ * @date 2019/3/5 19:56
+ * @description
+ */
+public class Q965 {
+//    965. 单值二叉树
+//    如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+//
+//    只有给定的树是单值二叉树时，才返回 true；否则返回 false。
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if(Objects.isNull(root)){
+            return true;
+        }
+        boolean result = true;
+        if(Objects.nonNull(root.left)){
+            result = result && root.val == root.left.val && isUnivalTree(root.left);
+        }
+        if(Objects.nonNull(root.right)){
+            result = result && root.val == root.right.val && isUnivalTree(root.right);
+        }
+        return result;
+    }
+}
