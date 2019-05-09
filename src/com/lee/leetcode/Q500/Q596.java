b93ca45dc7eb3e182dd63a91830c390531ac5d3c
Pwenlee <lipan@xinyunlian.com>
2019-05-09 17:25:32 +0800

    596

---
+++ src/com/lee/leetcode/Q500/Q596.java
@@ -0,0 +1,40 @@
+package com.lee.leetcode.Q500;
+
+/**
+ * @author Pwenlee
+ * @date 2019/5/9 17:24
+ * @description
+ */
+public class Q596 {
+//    596. 超过5名学生的课
+//    有一个courses 表 ，有: student (学生) 和 class (课程)。
+//
+//    请列出所有超过或等于5名学生的课。
+//
+//    例如,表:
+//
+//            +---------+------------+
+//            | student | class      |
+//            +---------+------------+
+//            | A       | Math       |
+//            | B       | English    |
+//            | C       | Math       |
+//            | D       | Biology    |
+//            | E       | Math       |
+//            | F       | Computer   |
+//            | G       | Math       |
+//            | H       | Math       |
+//            | I       | Math       |
+//            +---------+------------+
+//    应该输出:
+//
+//            +---------+
+//            | class   |
+//            +---------+
+//            | Math    |
+//            +---------+
+//    Note:
+//    学生在每个课中不应被重复计算。
+
+//select class from courses group by class having count(distinct(student)) >= 5;
+}
